91ff1dec9ab622fbc78cc820faf5dd2d23e2dcc2
yanglei <eaxebx@163.com>
2022-09-09 10:42:29 +0800

    add a test file

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "hellogo/testdatabase"
+import (
+	"fmt"
+	_ "hellogo/testdatabase"
+	"sync"
+)
 
 func main() {
-	testdatabase.Testdatabasemain()
+	var wg sync.WaitGroup
+	wg.Add(50)
+	wg.Add(50)
+	wg.Add(1)
+	for i := 0; i < 100; i++ {
+		go func(i int) {
+			fmt.Println(i)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
 }
--- test/waitfor.go
+++ test/waitfor.go
@@ -1,5 +1,12 @@
 package test
 
+/*
+	sync.WaitGroup
+	Add(n) 计数器加n
+	Done() 计数器减1
+	Wait() 等待,直到为0
+*/
+
 import (
 	"fmt"
 	"sync"
